96b770dad39c0409d63148ce763408cc823f9c86
Tim Pyrkov <tim.pyrkov@gmail.com>
2024-10-16 00:14:15 +0300

    upd aquamarine palette

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="excolor",
-    version="0.0.7",
+    version="0.0.8",
     author="Tim Pyrkov",
     author_email="tim.pyrkov@gmail.com",
     description="Extended colors for python",
